906308318dd8e88b31efb09ac0a2760177d833ab
rjforty <raaj.ahab@gmail.com>
2019-01-24 14:06:30 +0000

    Added exit code check for backup command

--- backup.sh
+++ backup.sh
@@ -1,18 +1,21 @@
 #! /bin/sh
 
 # Credentials to run database backup
-DATABASE_USERNAME=""
-DATABASE_PASSWORD=""
-DATABASE_NAME=""
+DATABASE_USERNAME="root"
+DATABASE_PASSWORD="pineapple"
+DATABASE_NAME="test"
+REMOTE_URL=""
 
 echo "Starting backup"
 echo "Please wait while backup is run.."
 
+# Running backup command with mysqldump utility
+mysqldump -u $DATABASE_USERNAME -p$DATABASE_PASSWORD --all-databases > backup.sql
 
-result=$(mysqldump -u $DATABASE_USERNAME -p$DATABASE_PASSWORD --all-databases > lala.sql)
-
-
-if [[ result == 0 ]]
+# Checking exit status of backup command
+if [ $? -eq 0 ]
     then
-        echo "Command passed"
+        echo "Database backup executed successfully"
+    else
+        echo "Database backup failed to execute"    
 fi
