aa7840ac1ddbf4a8ce99097b01712bd78f29837f
rjforty <raaj.ahab@gmail.com>
2019-01-25 13:45:51 +0000

    Compressing the backup file and sending it over a provided remote url

--- backup.sh
+++ backup.sh
@@ -4,10 +4,11 @@
 DATABASE_USERNAME="root"
 DATABASE_PASSWORD="pineapple"
 DATABASE_NAME="test"
-REMOTE_URL=""
+REMOTE_URL="http://localhost:8080/api/v1/backup"
 
 echo "Starting backup"
 echo "Please wait while backup is run.."
+
 
 # Running backup command with mysqldump utility
 mysqldump -u $DATABASE_USERNAME -p$DATABASE_PASSWORD --all-databases > backup.sql
@@ -17,5 +18,29 @@ if [ $? -eq 0 ]
     then
         echo "Database backup executed successfully"
     else
-        echo "Database backup failed to execute"    
+        echo "Database backup failed to execute"
+fi
+
+# After backup is done running, compressing the backup file
+tar -czvf backup.tar.gz backup.sql
+
+# Checking exit status of compression command
+if [ $? -eq 0 ]
+    then
+        echo "Backup compressed"
+    else
+        echo "Backup failed to compress"
+        exit
+fi
+
+# Transmitting file over network
+curl -v -F backup=@backup.tar.gz $REMOTE_URL
+
+# Checking exit status of compression command
+if [ $? -eq 0 ]
+    then
+        echo "File sent"
+    else
+        echo "Ooops file failed to send over network"
+        exit
 fi
